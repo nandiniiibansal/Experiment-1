48666e630447cb437d8af2a2b1fc5133bf5cf4e3
Nandini <bansal.nandini3103@gmail.com>
2024-08-11 22:53:52 +0530

    Delete exp2a.cpp

--- exp2a.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    char a[100];
-    cout<<"size of int "<<sizeof(int)<<" char "<<sizeof(char)<<" float "<<sizeof(float)
-    <<" double "<<sizeof(string)<<" long "<<sizeof(bool)<<" array "<<sizeof(a);
-    return 0;
-}
