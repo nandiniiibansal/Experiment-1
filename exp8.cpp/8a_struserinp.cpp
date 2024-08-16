1ab97a394ee81bb1dcd597186bf7155a5eecc382
Nandini <bansal.nandini3103@gmail.com>
2024-08-16 09:41:11 +0530

    Add files via upload

---
+++ exp8.cpp/8a_struserinp.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char s[]= "Nandini";
+    cout<<s<<endl;
+    return 0;
+}
---
+++ exp8.cpp/8d_palindromestr.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+int main() 
+{
+    string s1, s2;
+    cin>>s1;
+    s2=s1;
+    reverse(s1.begin(), s1.end());
+    if (s2==s1) 
+    {
+        cout<<"Yes! It is a palindrome";
+    }
+    else cout<<"No! It is not a palindrome";
+}
