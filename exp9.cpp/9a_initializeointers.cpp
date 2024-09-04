67879e3322bc4b794feee8409aaa6c9438392866
Nandini <bansal.nandini3103@gmail.com>
2024-09-04 18:46:58 +0530

    Add files via upload

---
+++ exp9.cpp/9a_initializeointers.cpp
@@ -0,0 +1,15 @@
+ #include <iostream>
+using namespace std;
+int main()
+{
+    int a = 10;
+    int *ptr;
+    ptr=&a;
+   cout<< "The value pointed by *ptr is: "<<*ptr << endl;
+    cout << "The value in b is: " <<a << endl;
+    cout << "The value in pointer variable ptr is: "<<(void*)ptr << endl;
+
+    cout << "the address of variable b is: "<<&a << endl;
+    ptr ++;
+    cout<< "After increment the value in pointer variable ptr is: " << (void*)ptr << endl;
+}
