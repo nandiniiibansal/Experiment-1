de4e11b79ff32afea6432f7c7a1d9999ca144f2a
Nandini <bansal.nandini3103@gmail.com>
2024-09-05 15:28:10 +0530

    Add files via upload

---
+++ exp12.cpp/12b_parameterizedconstr.cpp
@@ -0,0 +1,17 @@
+//PARAMETERIZED CONSTRUCTOR
+#include<iostream>
+using namespace std;
+class MyClass{
+    public:
+    int c;
+    MyClass(int val)
+{
+    c = val*val;
+    cout<<"Square of "<<val<<" is: "<<c<<endl;
+    
+}
+};
+int main() {
+    MyClass obj(9);
+    return 0;
+}
