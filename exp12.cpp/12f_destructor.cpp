ec867f79ecb8fe6e2e26b65c1d29ef5ffae37830
Nandini <bansal.nandini3103@gmail.com>
2024-09-12 14:42:50 +0530

    Add files via upload

---
+++ exp12.cpp/12f_destructor.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+class MyClass{
+    public:
+    ~MyClass(){
+        cout<<"Destructor called! "<< endl;
+    }
+    MyClass(){
+        cout<<"Constructor called!"<<endl;
+
+    }
+    };
+    int main(){
+        MyClass obj;
+        return 0;
+    }
